0219bda7498802de67157d1755c5c2c9529554aa
Andrew Korzhuev <wolfon@gmail.com>
2012-03-16 01:23:39 +0400

    Injecting MathJax when ready

--- inject.coffee
+++ inject.coffee
@@ -1,1 +1,19 @@
-alert "yay!!"
+inject = ->
+	return if MathJax?
+
+	config = document.createElement 'script'
+	config.setAttribute 'type', 'text/x-mathjax-config'
+	config.innerHTML = """
+			MathJax.Hub.Config({
+			  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+			});
+		"""
+
+	cdn = document.createElement 'script'
+	cdn.setAttribute 'type', 'text/javascript'
+	cdn.setAttribute 'src', 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+
+	document.head.appendChild config
+	document.head.appendChild cdn
+
+inject()
--- release/inject.js
+++ release/inject.js
@@ -1,5 +1,19 @@
 (function() {
+  var inject;
 
-  alert("yay!!");
+  inject = function() {
+    var cdn, config;
+    if (typeof MathJax !== "undefined" && MathJax !== null) return;
+    config = document.createElement('script');
+    config.setAttribute('type', 'text/x-mathjax-config');
+    config.innerHTML = "MathJax.Hub.Config({\n  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}\n});";
+    cdn = document.createElement('script');
+    cdn.setAttribute('type', 'text/javascript');
+    cdn.setAttribute('src', 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML');
+    document.head.appendChild(config);
+    return document.head.appendChild(cdn);
+  };
+
+  inject();
 
 }).call(this);
